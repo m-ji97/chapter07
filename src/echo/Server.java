03875d36f691b9ef43fe3c2f22a5cc2f7d646af3
m-ji97 <tv5035@naver.com>
2024-01-17 16:32:44 +0900

    24-01-17 수업

---
+++ src/echo/Server.java
@@ -0,0 +1,53 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException{
+
+		//서버소켓생성
+		ServerSocket serverSocket = new ServerSocket();
+		
+		//바인딩
+		serverSocket.bind(new InetSocketAddress("192.168.0.32", 10001));
+		
+		//서버시작
+		System.out.println("<서버시작>");
+		System.out.println("==================================================");
+		
+		//반복
+		while(true) {
+			System.out.println("[연결을 기다리고 있습니다.]");
+			
+			//클라이언트가 접속을 하면 accept()가 실행됨
+			Socket socket = serverSocket.accept();
+			
+			//출장   thread.start()
+			Thread thread = new ServerThread(socket);
+			thread.start();
+			
+		}
+	
+		/*
+		System.out.println("=======================================");
+		System.out.println("<서버 종료>");
+		
+		//닫기
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
+		*/
+	}
+
+}
